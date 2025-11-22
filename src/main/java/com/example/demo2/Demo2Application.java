47542e2f64cd5fe167468814daf5fa40ee7932ed
RITA\shaha <shahanaj.pervin0403@gmail.com>
2025-11-22 14:22:55 +0400

    demo2 cmt3

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo2Application {
     /// push from main
+    /// push from feature branch sample1
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
