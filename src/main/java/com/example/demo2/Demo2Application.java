5f1b96b6ec9e76cfc59be8f1eeac2a1a6a414690
ShahanajPervin <118610225+ShahanajPervin@users.noreply.github.com>
2025-11-22 14:27:38 +0400

    Update Demo2Application.java

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
     /// push from main
     /// push from feature branch sample1
+	/// pull from feature branch sample1
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
